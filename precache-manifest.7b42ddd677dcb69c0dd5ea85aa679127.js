ff0a21e6cbea6141b3da2183f9afead0cfcb3674
Josh Villanueva <77131654+JoshVL@users.noreply.github.com>
2022-06-13 20:01:45 -0500

    Updates

---
+++ precache-manifest.7b42ddd677dcb69c0dd5ea85aa679127.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e73570ff996d3737f3e0f566163892",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7288a0b73bf8d916eff7",
+    "url": "./static/css/main.7bf146b9.chunk.css"
+  },
+  {
+    "revision": "3837c85bfc94dcd7d15a",
+    "url": "./static/js/2.a13a3398.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a13a3398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7288a0b73bf8d916eff7",
+    "url": "./static/js/main.6f091e4e.chunk.js"
+  },
+  {
+    "revision": "8176c60e066153806f52",
+    "url": "./static/js/runtime-main.bc88bcec.js"
+  },
+  {
+    "revision": "ad69c8932765e54870e5bc37b4e98e79",
+    "url": "./static/media/Comfortaa-Light.ad69c893.woff"
+  },
+  {
+    "revision": "cd5084031ae15d7640e2e07b71f32930",
+    "url": "./static/media/Comfortaa-Regular.cd508403.woff"
+  },
+  {
+    "revision": "e5067b7c3b648f5f8fbeb3288b6d0124",
+    "url": "./static/media/duckduckgo.e5067b7c.svg"
+  },
+  {
+    "revision": "a8e8feea4a4be6163656cc40072070c7",
+    "url": "./static/media/google.a8e8feea.svg"
+  },
+  {
+    "revision": "3935eb7fb6a25bacb84de752d2b042f2",
+    "url": "./static/media/pic_1.3935eb7f.jpg"
+  },
+  {
+    "revision": "42b93d95319153fe54b47953aad4ed1b",
+    "url": "./static/media/pic_2.42b93d95.jpg"
+  },
+  {
+    "revision": "8796fde5bffabc5215bc0aad44d1a142",
+    "url": "./static/media/pic_3.8796fde5.jpg"
+  },
+  {
+    "revision": "66ccb182a56965ca730c5fecb7f7862a",
+    "url": "./static/media/pic_4.66ccb182.jpg"
+  },
+  {
+    "revision": "6e9543cd93106257d6783bd347446d27",
+    "url": "./static/media/pic_5.6e9543cd.jpg"
+  },
+  {
+    "revision": "65b20253b182ec977b99709892c72524",
+    "url": "./static/media/pic_6.65b20253.jpg"
+  },
+  {
+    "revision": "6516acf69228e1450d3b7c6f1ce7f475",
+    "url": "./static/media/pic_7.6516acf6.jpg"
+  },
+  {
+    "revision": "ba1342f42d643e4f554eaf120c6a2a38",
+    "url": "./static/media/pic_8.ba1342f4.png"
+  },
+  {
+    "revision": "f90c346a2cf4f445626a8b1211caa6df",
+    "url": "./static/media/qwant.f90c346a.svg"
+  }
+]);
